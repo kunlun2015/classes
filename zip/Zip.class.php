f9a423b62668020f75bed2d0be542be17195f6c8
kunlun2015 <725767227@qq.com>
2016-05-25 22:40:01 +0800

    分享一些常用类文件

---
+++ zip/Zip.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 解压缩包
+ * @authors Kunlun (735767227@qq.com)
+ * @date    2016-04-14 17:09:22
+ * @copyright Kunlun
+ */
+
+$zip = new ZipArchive; 
+$res = $zip->open('test.zip'); 
+if ($res === true) {     
+    //解压缩到test文件夹 
+    $zip->extractTo('test'); 
+    $zip->close(); 
+    echo 'ok'; 
+} else { 
+    echo 'failed, code:' . $res; 
+} 
---
+++ zip/test/Zip.class.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * 解压缩包
+ * @authors Kunlun (735767227@qq.com)
+ * @date    2016-04-14 17:09:22
+ * @copyright Kunlun
+ */
+
+$zip = new ZipArchive; 
+var_dump($zip);
